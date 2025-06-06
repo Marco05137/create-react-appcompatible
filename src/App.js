1111a3b89e388c59603a03025cbbd78cbc034e1b
Marco Antonio <tucorreo@ejemplo.com>
2025-06-05 21:13:54 -0400

    test visual basica en fronted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import React from 'react';
-
 function App() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>¡Hola Marco!</h1>
-      <p>Esto está funcionando correctamente 🟢</p>
+    <div>
+      <h1>Hola, ¡tu frontend está funcionando!</h1>
     </div>
   );
 }
